18ee8d15dcc6a0c7f66448e3e8428a9493f3dcd9
maneywu <mwu019@fiu.edu>
2021-09-25 13:35:35 -0400

    added !bye replies

--- bot code.py
+++ bot code.py
@@ -15,4 +15,9 @@ async def hi(ctx):
     greetings = ["Hello there!", "Hiya scrub", "Hi", "Want a shell?", "Test"]
     await ctx.send(random.choice(greetings))
 
+@client.command()
+async def bye(ctx):
+    farewells = ["Bye!", "See you later!", "Ciao!", "Goodbye!", "Adieu!"]
+    await ctx.send(random.choice(farewells))
+
 client.run(TOKEN)
