0952154a57592109f1378cafc9143dd8a2e589ca
Casey Sit <caseysit0714@gmail.com>
2021-09-25 14:22:38 -0400

    welcome user and intents recognized

--- bot code.py
+++ bot code.py
@@ -4,30 +4,38 @@ from discord.ext import commands
 
 TOKEN = open("token.txt","r").readline()
 
-client = commands.Bot(command_prefix= '!')
-
 intents = discord.Intents.default()
 intents.members = True
-intents.messages = True
-intents.guild = True
+intents.presences = True
 
-client2 = discord.Client(intents=intents)
+client = commands.Bot(command_prefix= '!', intents = intents)
 
+print(discord.__version__)
+
+@client.event
+async def on_ready():
+    print("Systems Online!")
 
 @client.event
 async def on_member_join(member):
-    channel = client2.get_channel(891084381681229837)
-    await channel.send("Welcome!" + "You are currently " + member.status)
-
+    channel = client.get_channel(891084381681229837)
+    await channel.send("Welcome " + member.mention + "! Enjoy your stay!")
 
 @client.event
-async def ready_msg():
-    print("Systems Online!")
+async def on_member_remove(member):
+    channel = client.get_channel(891084381681229837)
+    leaveMessage = discord.Embed
+    leaveMessage.setTitle("A user has left the guild")
+    leaveMessage.setMessage(client.get_user)
+    leaveMessage.set_image("testing.png")
+    await channel.send(leaveMessage)
 
+#Above is events, below is commands?
 
 @client.command()
 async def hi(ctx):
-    greetings = ["Hello there!", "Hiya scrub", "Hi", "Want a shell?", "Test"]
+    greetings = ["Hello there!", "Hiya scrub", "Hi", "Want a shell?", "Greetings!"]
     await ctx.send(random.choice(greetings))
+
 
 client.run(TOKEN)
